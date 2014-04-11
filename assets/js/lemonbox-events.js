072bd8250acf7b036988550e20a386c9aec1d951
Marcus Battle <marcus@marcusbattle.com>
2014-04-11 01:55:27 -0400

    added countdown highlight

--- assets/js/lemonbox-events.js
+++ assets/js/lemonbox-events.js
@@ -8,10 +8,15 @@
 
 		setInterval(function () {
 		    
+			var days_label = $('.event-countdown #days');
+		    var hours_label = $('.event-countdown #hours');
+		    var minutes_label = $('.event-countdown #minutes');
+		    var seconds_label = $('.event-countdown #seconds');
+
 		    // find the amount of "seconds" between now and target
 		    var current_date = new Date().getTime();
 		    var seconds_left = (target_date - current_date) / 1000;
-		 
+		 	
 		    // do some time calculations
 		    days = parseInt(seconds_left / 86400);
 		    seconds_left = seconds_left % 86400;
@@ -23,10 +28,22 @@
 		    seconds = parseInt(seconds_left % 60);
 		     
 		    // format countdown string + set tag value
-		    $('.event-countdown #days').text(days);
-		    $('.event-countdown #hours').text(hours);
-		    $('.event-countdown #minutes').text(minutes);
-		    $('.event-countdown #seconds').text(seconds);
+		    $(days_label).text(days);
+		    $(hours_label).text(hours);
+		    $(minutes_label).text(minutes);
+		    $(seconds_label).text(seconds);
+
+		    if ( $(days_label).text() == 0 ) {
+		    	
+		    	$(days_label).removeClass('label-primary').addClass('label-default');
+		    	$(hours_label).addClass('label-primary');
+
+		    } else if ( $(hours_label).text() == 0 ) {
+
+		    	$(hours_label).removeClass('label-primary').addClass('label-default');
+		    	$(minutes_label).addClass('label-primary');
+		    	
+		    }
 
 		}, 1000);
 
