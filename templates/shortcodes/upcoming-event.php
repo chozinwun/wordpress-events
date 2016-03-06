152eed009de83de24f474d7508bd5db0094fe6fc
root <root@east.battleideas.xyz>
2016-03-06 01:41:23 -0500

    Added extra css for support of ambassador theme

--- templates/shortcodes/upcoming-event.php
+++ templates/shortcodes/upcoming-event.php
@@ -4,7 +4,7 @@
    <div class="container">
       <div class="row">
          <?php if ( $upcoming_event && ( current_time('timestamp') < $upcoming_event->timestamp ) ): ?>
-            <div class="col-sm-5 col-md-5">
+            <div id="date-time" class="col-sm-5 col-md-5">
                <h4>Next Upcoming Event</h4>
                <h2>
                   <a href="<?php echo $upcoming_event->permalink ?>"><?php echo $upcoming_event->post_title ?></a>
@@ -12,7 +12,7 @@
                </h2>
                <p><?php $upcoming_event->excerpt; ?></p>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div id="countdown" class="col-md-4 col-sm-4 col-xs-12">
                <h4>Event Begins In</h4>
                <div class="event-countdown event-countdown-small" data-date="<?php echo $upcoming_event->mysql ?>" style="display: none;">
                   <div class="timer-col"><span id="days" class="label label-primary"></span><span class="timer-type">days</span></div>
@@ -20,14 +20,9 @@
                   <div class="timer-col"><span id="minutes" class="label label-default"></span><span class="timer-type">mins</span></div>
                   <div class="timer-col"><span id="seconds" class="label label-default"></span><span class="timer-type">secs</span></div>
                </div>
-            </div>
-             <div class="col-md-3 col-sm-3 col-xs-12 ">
-               <?php if( $upcoming_event->ticket_id ): ?>
-                  <a type="button" class="btn btn-lg btn-primary" href="<?php echo $upcoming_event->permalink ?>">Buy Ticket $<?php echo isset($upcoming_event->ticket->meta['product_price'][0]) ? round($upcoming_event->ticket->meta['product_price'][0]) : '' ?></a>
-               <?php else: ?>
-                  <p><a type="button" class="btn btn-primary btn-lg" style="margin-top: 29px;" href="<?php echo get_permalink( $upcoming_event->ID ) ?>">View Event Details</a></p>
-                  <p><a href="<?php echo home_url(); ?>/events" class="btn btn-link">View all events</a></p>
-               <?php endif; ?>
+               <div id="upcoming-event-permalinks" class="col-md-12 col-sm-12 col-xs-12 ">
+                  <span><a type="button" class="button medium btn btn-primary btn-lg" style="margin-top: 29px;" href="<?php echo get_permalink( $upcoming_event->ID ) ?>">View Event Details</a><a href="<?php echo home_url(); ?>/events" class="btn btn-link">View all events</a></span>
+               </div>
             </div>
 
          <?php elseif ( $upcoming_event && ( $upcoming_event->timestamp < current_time('timestamp') ) ): ?>
@@ -42,8 +37,7 @@
                <p><?php $upcoming_event->excerpt; ?></p>
             </div>
             <div class="col-md-3 col-sm-3 col-xs-12 ">
-               <p><a type="button" class="btn btn-primary btn-lg" style="margin-top: 29px;" href="<?php echo get_permalink( $upcoming_event->ID ) ?>">View Event Details</a></p>
-               <p><a href="<?php echo home_url(); ?>/events" class="btn btn-link">View all events</a></p>
+               <span><a type="button" class="button btn btn-primary btn-lg" style="margin-top: 29px;" href="<?php echo get_permalink( $upcoming_event->ID ) ?>">View Event Details</a><a href="<?php echo home_url(); ?>/events" class="btn btn-link">View all events</a></span>
             </div>
 
          <?php endif; ?>
